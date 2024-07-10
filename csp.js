f45898c46ecd8b8b4846908ba7e5db3d64e35c10
valkehh <albin_berg@yahoo.com>
2024-07-10 17:50:23 +0200

    fixed image

--- csp.js
+++ csp.js
@@ -3,7 +3,11 @@ const policies = {
   'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://maps.googleapis.com'],
   'child-src': ["'self'"],
   'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
-  'img-src': ["'self'", 'https://raw.githubusercontent.com'],
+  'img-src': [
+    "'self'",
+    'https://raw.githubusercontent.com',
+    'https://gist.githubusercontent.com/valkehh/',
+  ],
   'font-src': ["'self'"],
   'frame-src': ["'self'"],
   'connect-src': ["'self'", 'https://maps.googleapis.com'],
--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -37,7 +37,7 @@ export async function Header() {
             <img
               className={classes.logo}
               alt="Payload Logo"
-              src="https://svgshare.com/i/183g.svg"
+              src="https://gist.githubusercontent.com/valkehh/52b9c57ce5130ff46c1f3003fc9e4c6f/raw/2a080f723347e95045dfd7a0ec023c2d2ea7869e/elliott.svg"
             />
           </Link>
           <HeaderNav header={header} />
